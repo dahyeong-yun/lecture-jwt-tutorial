46aa8644ae8c767ad18277a95e85bbbe41c682a5
danny <gryb809@gmail.com>
2022-06-05 17:41:05 +0900

    :sparkles: Security 설정 추가

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # lecture-jwt-tutorial
 - 인프런 JWT 강의 학습용 repo
 
+# 강의별
+
+## 1강
+
+## 2강
+- Security 설정이 예전 버전이다. 5.7.0-M2 버전 부터 `WebSecurityConfigurerAdapter`이 deprecated 되었다.
+  - 이에 따라 이제는 상속을 받지 않고, 그냥 일반적인 @Configuration 클래스에서 HttpSecurity를 매개변수로 받는 Bean을 등록한다. 
+  - 참고 링크 :  <https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter>
+
 # 완강 이후 추가 사항
 - [ ] 강의 소스 부분의 테스트 코드를 추가하기
---
+++ src/main/java/me/polymorph/tutorial/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package me.polymorph.tutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/api/hello").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
