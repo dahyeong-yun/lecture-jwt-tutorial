a52ed41856fb52b1d9bffe4d1c8ed7c94bdc3e5f
danny <gryb809@gmail.com>
2022-06-05 23:35:39 +0900

    :sparkles: JWT 설정 추가

--- src/main/java/me/polymorph/tutorial/config/SecurityConfig.java
+++ src/main/java/me/polymorph/tutorial/config/SecurityConfig.java
@@ -1,22 +1,55 @@
 package me.polymorph.tutorial.config;
 
+import me.polymorph.tutorial.jwt.JwtAccessDeniedHandler;
+import me.polymorph.tutorial.jwt.JwtAuthenticationEntryPoint;
+import me.polymorph.tutorial.jwt.JwtSecurityConfig;
+import me.polymorph.tutorial.jwt.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 public class SecurityConfig {
 
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public SecurityConfig(TokenProvider tokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .exceptionHandling()
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+            .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+            .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .apply(new JwtSecurityConfig(tokenProvider))
+            .and()
                 .authorizeHttpRequests((authz) -> authz
                         .antMatchers("/api/hello").permitAll()
+                        .antMatchers("/api/authenticate").permitAll()
+                        .antMatchers("/api/signup").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+        ;
         return http.build();
     }
 
---
+++ src/main/java/me/polymorph/tutorial/jwt/JwtSecurityConfig.java
@@ -0,0 +1,24 @@
+package me.polymorph.tutorial.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Security 필터에 jwtFilter를 추가하는 역할
+ * */
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        JwtFilter jwtFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
