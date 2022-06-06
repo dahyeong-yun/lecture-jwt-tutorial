b7e12dfc792b311459ecb3f932e039d0870f01ac
danny <gryb809@gmail.com>
2022-06-06 14:21:47 +0900

    :sparkles: 회원 가입 API 및 권한 검증 추가

---
+++ src/main/java/me/polymorph/tutorial/util/SecurityUtil.java
@@ -0,0 +1,34 @@
+package me.polymorph.tutorial.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+@Slf4j
+public class SecurityUtil {
+
+    private SecurityUtil() {
+    }
+
+    public static Optional<String> getCurrentUsername() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // JWT doFilter의 실행 시점에 저장이 되어있음
+
+        if (authentication == null) {
+            log.debug("Security Context에 인증 정보가 없습니다.");
+            return Optional.empty();
+        }
+
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            username = springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+
+        return Optional.ofNullable(username);
+    }
+}
